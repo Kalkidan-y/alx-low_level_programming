8a96ca929c0ca44231987b74fb5354fa2cfde0ac
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-13 11:57:36 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
-#!/bin/bash
- #include<stdio.h>
-
-int main (void)
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Return: Always 0
+ */
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

39dbaf1b738f43ebe252284a7714b6a5d161b016
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-13 10:00:36 -0700

    hello c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,4 @@
- 
-
-/**
+ /**
  *
  *  * main - Entry point
  *
@@ -12,8 +10,8 @@
 
 #include <stdio.h>
 
-int main( )
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle,");
+return (0);
 }

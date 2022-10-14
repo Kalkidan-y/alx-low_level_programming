034b717551b97647d1b13b3d07b4baa38d66f142
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-14 16:37:05 -0700

    hi

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 #include <time.h>
 /**
- * more headers goes there
- * betty style doc for function main goes there
+ * main - prints wether the number is +,-, or 0
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

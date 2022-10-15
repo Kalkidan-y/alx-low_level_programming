a1ff6b909fb5ad339e1e3fdcd690263768169fe5
Kalkidan-y <105030339+Kalkidan-y@users.noreply.github.com>
2022-10-14 18:45:47 -0700

    Update 6-print_numberz.c
    hi!

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - print single digit numbers
  * Description: "prints all single digit numbers of base 10 starting from 0"
- * Return: 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {

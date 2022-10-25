285c89deb3f251e78bb40b22de6ef7674d7ccb44
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-25 13:42:34 -0700

    task0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -> a functio to update the reference of a pointer.
+ * @n: pointer to n to be pdated.
+ * Return: A pointer to the destination string @dest.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

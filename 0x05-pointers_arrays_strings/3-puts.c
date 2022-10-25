658d30788726a9ce1262fc362a24ff9a1596f8e9
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-25 16:03:16 -0700

    task 4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,14 @@
 #include "main.h"
 
-/*
- * _puts - is a function that prints string.
- * @str: a parameter to _puts function
- *
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
  */
 
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar (*str);
-	}
+	while (*str)
+		_putchar(*str++);
+
 	_putchar('\n');
 }

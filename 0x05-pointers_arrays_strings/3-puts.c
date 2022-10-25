13507c20e4a45e8ae0d658fd6c97b0c82c134875
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-25 15:00:21 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 
 /*
  * _puts -> this is a function that puts
- * @str: a parameer
+ * @str: a parameter to _puts function
  *
  */
 
@@ -12,5 +12,5 @@ void _puts(char *str)
 	{
 		_putchar (*str);
 	}
-	_putchar('\n')
+	_putchar('\n');
 }

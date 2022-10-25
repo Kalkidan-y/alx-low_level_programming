92510ba917f872c4ebdda0b573e0c0dcffd69b2b
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-10-25 16:59:35 -0700

    hi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
+ * _atoi -> Converts a string to an integer.
  * @s: The string to be converted.
  *
+ * Return: multiplication of sum and sign.
  */
 
 int _atoi(char *s)

398f4136875f98085da5efe99d59f14705139add
kalkidan-y <kalkidanyibeltal55@gmail.com>
2022-11-14 09:48:10 -0800

    the preprocessor task

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+/**
+ *
+ * main - check the code
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ */
+
+int main(void)
+{
+	printf("name of the file it was compiled from is %s\n",__FILE__);
+
+	return(0);
+}
